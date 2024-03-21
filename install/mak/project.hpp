95e187a972f3fc7761251095b599cca75741b2ae
Victor Bogado da Silva Lins <victor@bogado.net>
2024-03-21 09:16:44 -0400

    Starting to build a stage logic to the new 'mak'

--- install/mak/project.hpp
+++ install/mak/project.hpp
@@ -1,5 +1,8 @@
+#include "stage.hpp"
+
 #include <filesystem>
 #include <concepts>
+#include <optional>
 #include <utility>
 #include <array>
 #include <ranges>
@@ -9,66 +12,40 @@ namespace vb::mak {
 
 namespace prj {
 
-enum class stage : signed short {
-    FAILED = -1,
-    CONFIG,
-    BUILD,
-    INVALID,
-    DONE = 128,
-};
-
-enum class status : signed short {
-    FAILED = -1,
-    PENDING,
-    STARTED,
-    DONE = 128,
-};
-
-template <typename ENUM_T>
-requires std::is_enum_v<ENUM_T>
-constexpr inline auto operator <=>(const stage& a, const stage& b) {
-    return static_cast<std::underlying_type_t<ENUM_T>>(a) <=> static_cast<std::underlying_type_t<ENUM_T>>(b);
-};
-
-static_assert(stage::CONFIG < stage::DONE);
-static_assert(status::DONE > status::FAILED);
-
-constexpr auto STAGES = static_cast<unsigned>(stage::INVALID);
-
-struct state {
-    std::array<status, STAGES> stages;
-
-    constexpr status operator[](stage st) const {
-        return stages[static_cast<unsigned short>(st)];
-    }
-
-    constexpr stage next() const {
-        auto r = std::ranges::find_if(stages, [](const auto& s) -> bool {
-            return s > status::DONE;
-        });
-
-        if (r != stages.end()) {
-            return *r == status::DONE
-                ? stage::DONE
-                : stage::FAILED;
-        } else {
-            return static_cast<stage>(std::distance(stages.begin(), r));
-        }
-    }
-};
-    
 template <typename PROJECT_T>
 concept is_project = std::is_default_constructible_v<PROJECT_T> 
     && std::constructible_from<std::filesystem::path>
     && requires (const PROJECT_T prj, std::filesystem::path root)
 {
     { prj.root() } ->  std::same_as<std::filesystem::path>;
-    { prj.operator() } -> std::convertible_to<bool>;
-    { prj.status() } -> std::same_as<status>;
+    { prj.file(std::string{}) } -> std::same_as<std::optional<std::filesystem::path>>;
 };
 
 struct basic_project
 {
+    using path = std::filesystem::path;
+
+    path root_folder;
+
+    basic_project(path r) :
+        root_folder{r}
+    {}
+
+    path root() const noexcept
+    {
+        return root_folder;
+    }
+
+
+    std::optional<path> file(std::string name) const noexcept
+    {
+        auto file = (root_folder / name);
+        if (!std::filesystem::is_regular_file(file))
+        {
+            return std::nullopt;
+        }
+        return file;
+    }
 };
 
 }
---
+++ install/mak/stage.hpp
@@ -0,0 +1,100 @@
+#include "util/execution.hpp"
+#include "util/string.hpp"
+#include <concepts>
+#include <cstdint>
+#include <type_traits>
+#include <util/filesystem.hpp>
+#include <ranges>
+
+namespace vb::mak {
+
+struct stage {
+    enum class state : std::int8_t {
+        PENDING = -1,
+        INVALID = 0,
+        STARTED,
+        DONE,
+        BLOCKED,
+        NOT_REQUIRED,
+        FAILED
+    };
+
+    virtual ~stage() = default;
+    virtual fs::path root() const = delete;
+    virtual fs::path work_dir() const = delete;
+    virtual state status() const = delete;
+    virtual void run() = delete;
+};
+
+template <typename STAGE_T>
+concept is_stage = requires(const STAGE_T stage, STAGE_T mutable_stage) {
+    requires std::constructible_from<STAGE_T, fs::path>; // This is the root of the project.
+    { stage.root() } -> std::same_as<fs::path>;
+    { stage.work_dir() } -> std::same_as<fs::path>;
+    { stage.status() } -> std::same_as<stage::state>;
+    { mutable_stage.run() };
+};
+
+template <is_stage IMPL>
+auto make_stage(IMPL&& impl) {
+    struct stage_impl final : stage {
+        IMPL impl;
+
+        stage_impl(fs::path root) :
+            impl{root}
+        {}
+
+        fs::path work_dir() const override {
+            return impl.work_dir();
+        }
+
+        stage::state status() const override {  
+            return impl.status();
+        }
+
+        void run() override {
+            impl.run();
+        }
+    };
+    return stage_impl{std::move(impl)};
+}
+
+template <static_string COMMAND>
+struct basic_stage {
+    using state = stage::state;
+    using enum state;
+
+    stage::state current_status = PENDING;
+    fs::path prj_root;
+
+    basic_stage(fs::path _root) :
+        prj_root{_root}
+    {}
+
+    auto root() const {
+        return prj_root;
+    }
+
+    auto work_dir() const {
+        return root();
+    }
+
+    auto status() const {
+        return current_status;
+    }
+
+    void run() {
+        current_status = STARTED;
+        auto run = execution{io_set::NONE};
+        run.execute(std::string{COMMAND}, work_dir());
+        auto result = run.wait();
+        current_status = result == 0 ? DONE : FAILED;
+    }       
+};
+
+namespace test {
+
+    using namespace vb::literals;
+
+}
+}
