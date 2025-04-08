c535f7c42a84abf5668a13cc19d08d9451ef99df
Victor Bogado da Silva Lins <victor@bogado.net>
2025-04-08 09:36:23 -0400

    Missed builders

---
+++ install/mak/builder.hpp
@@ -0,0 +1,241 @@
+#ifndef INCLUDED_BUILDER_HPP
+#define INCLUDED_BUILDER_HPP
+
+#include "./work_directory.hpp"
+#include "./result.hpp"
+
+#include <util/environment.hpp>
+#include <util/filesystem.hpp>
+#include <util/string.hpp>
+
+#include <concepts>
+#include <filesystem>
+#include <memory>
+#include <optional>
+
+
+namespace vb::maker {
+
+template<typename RUNNER>
+concept is_runner = requires(const RUNNER runner) {
+    { runner.working_directory() } -> std::same_as<fs::path>;
+    { runner.run() } -> std::same_as<bool>;
+};
+
+template <typename OPT_RUNNER>
+concept is_optional_runner =
+    std::same_as<std::nullopt_t, OPT_RUNNER> || requires {
+      requires is_runner<typename OPT_RUNNER::value_type>;
+      requires std::same_as<OPT_RUNNER,
+                            std::optional<typename OPT_RUNNER::value_type>>;
+    };
+
+struct builder_base {
+  using ptr = std::unique_ptr<builder_base>;
+  using factory = ptr (*)(work_dir, env::environment::optional);
+
+  virtual ~builder_base() = default;
+
+  virtual std::string name() const = 0;
+  virtual work_dir root() const = 0;
+  virtual bool required() const = 0;
+  virtual ptr next_builder() const { return nullptr; }
+
+  virtual fs::path working_directory() const { return root().path(); }
+  virtual execution_result execute() const = 0;
+};
+
+template <typename BUILDER>
+concept is_builder =
+    is_runner<BUILDER> && std::derived_from<builder_base, BUILDER> &&
+    requires(const fs::path path) {
+      { BUILDER::is_root(path) } -> std::same_as<bool>;
+    };
+
+template<typename SPEC_T>
+concept is_builder_spec = requires {
+    requires is_string<decltype(SPEC_T::name)>;
+    requires is_string<decltype(SPEC_T::build_file)>;
+    requires is_string<decltype(SPEC_T::command)>;
+};
+
+template <is_builder_spec SPECIFICATION, typename CLASS = void>
+struct basic_builder : builder_base {
+private:
+  work_dir root_wd;
+  env::environment environment{};
+
+public:
+  static bool is_root(work_dir root) {
+    return root.has_file(SPECIFICATION::build_file);
+  }
+
+  static constexpr basic_builder::factory create =
+      [](work_dir dir, env::environment::optional env) {
+        if (!is_root(dir)) {
+          return basic_builder::ptr{nullptr};
+        }
+
+        if constexpr (std::same_as<CLASS, void>) {
+          return basic_builder::ptr{new basic_builder{dir, env}};
+        } else {
+          return basic_builder::ptr{new CLASS{dir, env}};
+        }
+      };
+
+  static constexpr auto command = SPECIFICATION::command;
+  static constexpr auto build_file = SPECIFICATION::build_file;
+
+  explicit basic_builder(work_dir rt, env::environment::optional env_)
+      : root_wd{rt}, environment{env_.value_or(env::environment{})} 
+  {
+      if constexpr (requires { SPECIFICATION::import_vars; })
+      {
+          for (auto var_name : SPECIFICATION::import_vars) {
+              env().import(var_name);
+          }
+      }
+      for (auto var_name : std::array{ "HOME", "USER", "USERNAME", "UID", "PATH"} ) {
+          env().import(var_name);
+      }
+  }
+
+  std::string name() const override {
+    return vb::to_string(SPECIFICATION::name);
+  }
+
+  work_dir root() const override { return root_wd; }
+
+  basic_builder::ptr next_builder() const override { return nullptr; }
+
+  bool required() const override { return true; }
+
+  virtual std::vector<std::string> arguments() const {
+    if constexpr (requires { SPECIFICATION::arguments; }) {
+      return std::ranges::to<std::vector>(
+          SPECIFICATION::arguments | std::views::transform([](const auto &arg) {
+            return vb::to_string(arg);
+          }));
+    } else {
+      return std::vector<std::string>{};
+    }
+  }
+
+  execution_result execute() const override {
+    return root().execute(command, arguments(), env());
+  }
+
+protected:
+  const auto &env() const { return environment; }
+
+  auto &env() { return environment; }
+};
+
+struct builder : builder_base
+{
+private:
+    builder_base::ptr impl;
+
+public:
+    using optional = std::optional<builder>;
+
+    constexpr builder() = default;
+
+    builder(builder_base::ptr&& impl_)
+        : impl{std::move(impl_)}
+    {}
+
+    explicit operator bool() const {
+        return impl != nullptr;
+    }
+
+    execution_result run() const
+    {
+        if (!required()) {
+            return execution_result::NOT_NEEDED;
+        }
+
+        return execute();
+    }
+
+
+    std::string name() const override {
+        if (!*this) {
+            return "«NO BUILDER»";
+        }
+
+        return impl->name();
+    }
+
+    bool required() const override {
+        if (!*this) {
+            return false;
+        }
+
+        return impl->required();
+    }
+
+    execution_result execute() const override
+    {
+        if (!*this) {
+            return execution_result::PANIC;
+        }
+
+        return impl->execute();
+    }
+
+    work_dir root() const override 
+    {
+        if (!*this) {
+            return work_dir{};
+        }
+
+        return impl->root();
+    }
+
+    ptr next_builder() const override
+    {
+        if(!*this) {
+            return nullptr;
+        }
+
+        return impl->next_builder();
+    }
+
+    builder next() const
+    {
+        if (*this) {
+            if (auto nxt = impl->next_builder(); nxt != nullptr) {
+                return builder{std::move(nxt)};
+            }
+        }
+        return builder{};
+    }
+
+    fs::path working_directory() const override
+    {
+        return impl->working_directory();
+    }
+};
+} // namespace vb::maker
+
+template <>
+struct std::formatter<vb::maker::builder, char>
+{
+    template <class PARSE_CONTEXT>
+    constexpr PARSE_CONTEXT::iterator parse(PARSE_CONTEXT& context)
+    {
+        return context.begin();
+    }
+
+    template <class FORMATTER_CONTEXT>
+    FORMATTER_CONTEXT::iterator format(const vb::maker::builder& builder, FORMATTER_CONTEXT& context) const
+    {
+        auto out = context.out();
+        out = std::ranges::copy("🔧 "sv, out).out;
+        out = std::ranges::copy(builder.name(), context.out()).out;
+        return out;
+    }
+};
+
+#endif // INCLUDED_BUILDER_HPP
---
+++ install/mak/builders.hpp
@@ -0,0 +1,54 @@
+#ifndef INCLUDED_BUILDERS_HPP
+#define INCLUDED_BUILDERS_HPP
+
+#include "builder.hpp"
+#include "builders/conan.hpp"
+#include "builders/cmake.hpp"
+#include "builders/ninja.hpp"
+
+#include <string_view>
+#include <util/environment.hpp>
+
+namespace vb::maker::builders {
+
+using namespace vb::literals;
+using namespace std::literals;
+
+struct jekyll_spec {
+    static constexpr std::string_view name = "Jekyll";
+    static constexpr std::string_view build_file = "_config.yml";
+    static constexpr std::string_view command = "jekyll";
+    static constexpr std::array arguments = { "build"sv };
+};
+
+using jekyll = basic_builder<jekyll_spec>;
+
+struct make_spec {
+    static constexpr std::string_view name = "make";
+    static constexpr std::string_view build_file = "Makefile";
+    static constexpr std::string_view command = "make";
+};
+
+using make = basic_builder<make_spec>;
+
+inline const auto all_factories = std::array{
+    make::create,
+    conan::create,
+    cmake::create,
+    ninja::create,
+    jekyll::create
+};
+
+builder_base::ptr select(work_dir root, env::environment::optional env = {}) {
+    for (const auto& factory: all_factories) {
+        if(auto ptr = factory(root, env); ptr != nullptr)
+        {
+            return ptr;
+        }
+    }
+    return builder_base::ptr{nullptr};
+}
+
+}
+
+#endif // INCLUDED_BUILDERS_HPP
